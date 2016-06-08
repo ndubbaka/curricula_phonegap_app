06a9840c84cfeb09a28db39970e6b8eca1c0af70
Nikhil Dubbaka <info@technikh.com>
2016-06-08 17:06:08 -0400

    testing youtube.

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/remcob00-cordova-youtube-android-player-api/www/youtube.js",
+        "id": "remcob00-cordova-youtube-android-player-api.youtube",
+        "pluginId": "remcob00-cordova-youtube-android-player-api",
+        "clobbers": [
+            "youtube"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "remcob00-cordova-youtube-android-player-api": "2.0.6"
+}
+// BOTTOM OF METADATA
+});
